49b1c88b06ae71e0598951d0e5e0a35747ebd366
Jony Karki <jonykarki@hotmail.com>
2017-10-22 15:47:17 +0545

    initial commit

--- app.js
+++ app.js
@@ -1,78 +1,35 @@
 // This loads the environment variables from the .env file
 require('dotenv-extended').load();
 
-var builder = require('botbuilder');
-var restify = require('restify');
+const builder = require('botbuilder');
+const restify = require('restify');
 
-// Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url);
+// we need the connector for our bot
+const connector = new builder.ChatConnector({
+    appId = process.env.MICROSOFT_APP_ID,
+    appPassword = process.env.MICROSOFT_APP_PASSWORD
 });
+const bot = new builder.UniversalBot(connector,
+    [
+        //ask the user for their name and greet them with that
+        (session) => {
+            builder.Prompts.text(session, "Hello There! What is your name?");
+        },
+        (session, results) => {
+            var name = results.response;
+            console.log(name);
+            session.endDialog('Hello, ' + name + ' how are you?');
+        }
+    ]
+);
 
-// create the bot and listen for the new messages
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
+// start the server
+const server = restify.createServer();
 
-//listen to the following api end point
+// we want to get the messaages in /api/messages and get the connector to listen
 server.post('/api/messages', connector.listen());
 
-// store all the users in the folowing array
-var userStore = [];
-var bot = new builder.UniversalBot(connector, function (session) {
-
-    // whenever there's a new cconnection store that address
-    var address = session.message.address;
-    userStore.push(address);
-
-    // end current dialog
-    session.endDialog('You\'ll Now be redirected to the Premium Chat Room....');
-});
-
-// Every 5 seconds, check for new registered users and start a new dialog
-setInterval(function () {
-    var newAddresses = userStore.splice(0);
-    newAddresses.forEach(function (address) {
-
-        console.log('Starting survey for address:', address);
-
-        // new conversation address, copy without conversationId
-        var newConversationAddress = Object.assign({}, address);
-        delete newConversationAddress.conversation;
-
-        // start survey dialog
-        bot.beginDialog(newConversationAddress, 'survey', null, function (err) {
-            if (err) {
-                // error ocurred while starting new conversation. Channel not supported?
-                bot.send(new builder.Message()
-                    .text('This channel does not support this operation: ' + err.message)
-                    .address(address));
-            }
-        });
-
-    });
-}, 5000);
-
-bot.dialog('survey', [
-    function (session) {
-        builder.Prompts.text(session, 'Hey There! What is your name?');
-    },
-    function (session, results) {
-        // get the name of the user and save i
-        var name = results.response;
-        session.userData.name = results.response;
-        builder.Prompts.number(session, 'Hi ' + name + ', How many years have you been coding?');
-    },
-    function (session, results) {
-        session.userData.coding = results.response;
-        builder.Prompts.choice(session, 'What language do you code Node using? ', ['Java', 'Python', 'JavaScript']);
-    },
-    function (session, results) {
-        session.userData.language = results.response.entity;
-        session.endDialog('I Got it... ' + session.userData.name +
-            ' You have been programming for ' + session.userData.coding +
-            ' years and use ' + session.userData.language + ' as the main programming language.');
-    }
-]);
+// let's get the server to listen to a port
+server.listen(
+    process.env.PORT || 3978,
+    () => console.log("Server is running *:.."));
